1d23056e4cc50bfe4971958e0979ec901e5e40a8
Derek Miller <dgmxm7@gmail.com>
2019-09-18 15:25:12 -0500

    gitignore __pycache__

---
+++ test_linear_regression.py
@@ -0,0 +1,49 @@
+### Test file for linear_regression ###
+
+import numpy as np
+from linear_regression import *
+
+
+def test_simulate_data():
+    """
+    Ensures that the simulate_data method returns data.
+
+    """
+    raise ValueError("Test not implemented")
+
+
+def test_compare_models():
+    """
+    Test compare_models function.
+
+    """
+    raise ValueError("Test not implemented")
+
+
+def test_load_hospital_data():
+    """
+    Test load_hospital_data function.
+
+    """
+    raise ValueError("Test not implemented")
+
+
+
+def test_prepare_data():
+    """
+    Test prepare_data function.
+
+    """
+    raise ValueError("Test not implemented")
+
+
+# Tests on simulated data sets
+print("testing...")
+test_simulate_data()
+test_compare_models()
+test_load_hospital_data()
+test_prepare_data()
+print("\nSUCCESS!\n")
+
+
+### END ###
