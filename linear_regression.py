00f7a64c682dc2c4048eef1065e249fb08a1bd77
Li <Maggie.Li@chicagobooth.edu>
2019-09-19 13:16:33 -0500

    I copied linear_regression.py from

--- linear_regression.py
+++ linear_regression.py
@@ -11,10 +11,8 @@ def simulate_data():
     Simulates data for testing linear_regression models.
     INPUT
         nobs (int) the number of observations in the dataset
-
     RETURNS
         data (dict) contains X, y, and beta vectors.
-
     """
     pass
 
@@ -25,10 +23,8 @@ def compare_models():
     INPUT
         X (ndarray) the independent variables in matrix form
         y (array) the response variables vector
-
     RETURNS
         results (pandas.DataFrame) of estimated beta coefficients
-
     """
     pass
 
@@ -38,10 +34,8 @@ def load_hospital_data():
     Loads the hospital charges data set found at data.gov.
     INPUT
         path_to_data (str) indicates the filepath to the hospital charge data (csv)
-
     RETURNS
         clean_df (pandas.DataFrame) containing the cleaned and formatted dataset for regression
-
     """
     pass
 
@@ -51,10 +45,8 @@ def prepare_data():
     Prepares hospital data for regression (basically turns df into X and y).
     INPUT
         df (pandas.DataFrame) the hospital dataset
-
     RETURNS
         data (dict) containing X design matrix and y response variable
-
     """
     pass
 
@@ -64,10 +56,8 @@ def run_hospital_regression():
     Loads hospital charge data and runs OLS on it.
     INPUT
         path_to_data (str) filepath of the csv file
-
     RETURNS
         results (str) the statsmodels regression output
-
     """
     pass
  
